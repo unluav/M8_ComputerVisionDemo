0f52db272dd6fa929021e30b83010e574c3d08b7
Jake Koperski <wijakoperski@gmail.com>
2018-09-22 20:26:16 -0500

    Resolved video file path error and added /bin and /build to .gitignore

--- main.cpp
+++ main.cpp
@@ -4,12 +4,30 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
-	VideoCapture cap = VideoCapture("./video.mp4");
+	cout << "Demo Started" << endl;
 
-	while (cap.read(frame)) {
-        imshow("", frame);
-		if (waitKey(1) >= 0) break;
+	// Create the capture mechanism for reading a video and the matrix for each frame
+	// The path to the video must correspond to the path of the executable in ~/M8_ComputerVisionDemo/bin
+	VideoCapture capture("../video.mp4");
+	Mat frame;
+
+	// Handle when the capture mechanism is not correctly initialized
+	if (!capture.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return -1;
 	}
-    
+
+	// While there is another frame to read
+	while (capture.read(frame)) {
+		// Display the frame
+	        imshow("Computer Vision Demo", frame);
+		
+		// Press any key to exit the demo
+		if (waitKey(25) > 0) {
+			break;
+		}
+	}
+
+	cout << "Demo Complete" << endl;    
 	return 0;
 }
